35212890b61317a3b9923cda2237c0ede71d41c9
Bart Römgens <bart.romgens@gmail.com>
2015-02-27 01:25:41 +0100

    Fix indentation

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'voting.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from website.views import HomeView
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = patterns('',
+                       url(r'^$', HomeView.as_view()),
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
