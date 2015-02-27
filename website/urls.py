52142fdd996b96301bdb623c28a3b96a99733599
Bart Römgens <bart.romgens@gmail.com>
2015-02-27 03:15:23 +0100

    Add votes to bill context

--- website/urls.py
+++ website/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from website.views import HomeView
-from website.views import VotingsView
+from website.views import BillsView
 
 urlpatterns = patterns('',
                        url(r'^$', HomeView.as_view()),
-                       url(r'^uitslagen/', VotingsView.as_view()),
+                       url(r'^uitslagen/', BillsView.as_view()),
                        url(r'^admin/', include(admin.site.urls)),
                        )
--- website/views.py
+++ website/views.py
@@ -4,6 +4,8 @@ logger = logging.getLogger(__name__)
 
 from django.views.generic import TemplateView
 
+from voting.models import Bill
+
 
 class HomeView(TemplateView):
     template_name = "website/index.html"
@@ -14,10 +16,15 @@ class HomeView(TemplateView):
         return context
 
 
-class VotingsView(TemplateView):
-    template_name = "website/votings.html"
-    context_object_name = "votings"
+class BillsView(TemplateView):
+    template_name = "website/bills.html"
+    context_object_name = "bills"
 
     def get_context_data(self, **kwargs):
-        context = super(VotingsView, self).get_context_data(**kwargs)
+        context = super(BillsView, self).get_context_data(**kwargs)
+
+        bills = Bill.objects.all()
+        for bill in bills:
+            bill.votes = bill.get_votes()
+        context['bills'] = bills
         return context
