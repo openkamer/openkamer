23958798e267e2ab38ba1b6a64c695ebc185f4e5
Bart Römgens <bart.romgens@gmail.com>
2015-02-27 01:25:03 +0100

    Initial homepage

---
+++ website/views.py
@@ -0,0 +1,14 @@
+
+import logging
+logger = logging.getLogger(__name__)
+
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = "website/index.html"
+    context_object_name = "homepage"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        return context
