e4f813f2df05d3189fdb76b41c5a52171d52007d
Bart Römgens <bart.romgens@gmail.com>
2015-02-27 03:14:41 +0100

    Change Bill date field to datetime

--- voting/models.py
+++ voting/models.py
@@ -2,6 +2,8 @@
 import logging
 logger = logging.getLogger(__name__)
 
+from datetime import datetime
+
 from django.db import models
 
 
@@ -37,8 +39,11 @@ class Bill(models.Model):
     original_title = models.CharField(max_length=500, blank=True, null=False, default='')
     author = models.ForeignKey(Member)
     type = models.CharField(max_length=2, choices=TYPES)
-    date = models.DateField(auto_now=True)
+    datetime = models.DateTimeField(default=datetime.now, editable=True, blank=True)
     document_url = models.URLField(blank=True, null=False, default='')
+
+    def get_votes(self):
+        return Vote.objects.filter(bill=self)
 
     def __str__(self):
         return self.title + ' (' + str(self.author) + ')'
