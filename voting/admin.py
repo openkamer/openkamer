87b7a17e22b0d6261e583e56998b6c4fb8d94f54
Bart Römgens <bart.romgens@gmail.com>
2015-02-27 02:31:31 +0100

    Initial Party, Member, Bill and Vote admin

--- voting/admin.py
+++ voting/admin.py
@@ -4,3 +4,31 @@ logger = logging.getLogger(__name__)
 
 from django.conf import settings
 from django.contrib import admin
+
+from voting.models import Bill, Vote, Member, Party
+
+
+class BillAdmin(admin.ModelAdmin):
+    model = Bill
+    list_display = ('title', 'author', 'type', 'date', 'date')
+
+
+class MemberAdmin(admin.ModelAdmin):
+    model = Member
+    list_display = ('name', 'party')
+
+
+class PartyAdmin(admin.ModelAdmin):
+    model = Party
+    list_display = ('name', 'seats')
+
+
+class VoteAdmin(admin.ModelAdmin):
+    model = Vote
+    list_display = ('bill', 'party', 'decision', 'details')
+
+
+admin.site.register(Bill, BillAdmin)
+admin.site.register(Member, MemberAdmin)
+admin.site.register(Party, PartyAdmin)
+admin.site.register(Vote, VoteAdmin)
