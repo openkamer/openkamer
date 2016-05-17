5ad509d90caec1aecc87382caf39370e950f3f38
Bart Römgens <bart.romgens@gmail.com>
2016-05-17 09:46:28 +0200

    Create basic parties view and template

--- parliament/views.py
+++ parliament/views.py
@@ -1,3 +1,13 @@
 from django.views.generic import TemplateView
 
+from parliament.models import PoliticalParty
 
+
+class PartiesView(TemplateView):
+    template_name = 'parliament/parties.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        parties = PoliticalParty.objects.all()
+        context['parties'] = parties
+        return context
--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from person.views import PersonsView
+from parliament.views import PartiesView
 
 from website.views import HomeView
 from website.views import BillsView
@@ -11,6 +12,7 @@ import website.api
 urlpatterns = [
     url(r'^$', HomeView.as_view()),
     url(r'^persons/', PersonsView.as_view()),
+    url(r'^parties/', PartiesView.as_view()),
     url(r'^bills/', BillsView.as_view()),
     url(r'^api/', include(website.api)),
     url(r'^admin/', include(admin.site.urls)),
