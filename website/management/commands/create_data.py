5d3c05506c9d28435794c9cd7f45f417057970e7
Bart Römgens <bart.romgens@gmail.com>
2016-05-23 17:20:55 +0200

    Add demo dossiers in create_data command

--- website/management/commands/create_data.py
+++ website/management/commands/create_data.py
@@ -3,9 +3,17 @@ from django.core.management.base import BaseCommand
 import scraper.political_parties
 import scraper.parliament_members
 
+from person.models import Person
+from document.models import create_or_update_dossier
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         scraper.political_parties.create_parties()
         scraper.parliament_members.create_members()
+        Person.update_persons_all('nl')
+
+        # add a few demo dossiers
+        create_or_update_dossier('33885')
+        create_or_update_dossier('34344')
