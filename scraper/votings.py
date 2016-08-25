13240e189537e589f09045c73073083371b9d91f
Bart Römgens <bart.romgens@gmail.com>
2016-08-25 13:05:41 +0200

    improve comments and code cleanup

--- scraper/votings.py
+++ scraper/votings.py
@@ -20,7 +20,6 @@ class Vote(object):
 
     def create(self):
         ncol = 0
-
         for column in self.vote_table_row.iter():
             if column.tag == 'td':
                 ncol += 1
@@ -39,18 +38,10 @@ class Vote(object):
         return 'Vote: ' + self.party_name + ' (' + str(self.number_of_seats) + '): ' + self.decision
 
 
-class VotingResults(object):
+class VotingResult(object):
     def __init__(self, result_tree):
-        print('create VotingResults')
         self.result_tree = result_tree
         self.votes = self.create_votes_from_table()
-
-    def __str__(self):
-        return 'Voting for doc ' + self.get_document_id() + ', result: ' + self.get_result()
-
-    def print_votes(self):
-        for vote in self.votes:
-            print(vote)
 
     def get_property_elements(self):
         return self.result_tree.xpath('div[@class="search-result-properties"]/p')
@@ -89,6 +80,13 @@ class VotingResults(object):
         result_content_elements = self.result_tree.xpath('div[@class="search-result-content"]/p[@class="result"]/span')
         return result_content_elements[0].text.replace('.', '')
 
+    def __str__(self):
+        return 'Voting for doc ' + self.get_document_id() + ', result: ' + self.get_result()
+
+    def print_votes(self):
+        for vote in self.votes:
+            print(vote)
+
 
 def get_voting_pages_for_dossier(dossier_nr):
     """ searches for votings within a dossier, returns a list of urls to pages with votings """
@@ -103,19 +101,22 @@ def get_voting_pages_for_dossier(dossier_nr):
     elements = tree.xpath('//div[@class="search-result-content"]/h3/a')
     voting_urls = []
     for element in elements:
-        voting_url = TWEEDEKAMER_URL + element.get('href')
-        voting_urls.append(voting_url)
+        voting_urls.append(TWEEDEKAMER_URL + element.get('href'))
     return voting_urls
 
 
 def get_votings_for_page(votings_page_url):
-    """ scrapes votings from a votings page (example: https://www.tweedekamer.nl/kamerstukken/stemmingsuitslagen/detail?id=2016P10154) """
+    """
+    get voting results from a votings page
+    :param votings_page_url: the url of the votings page, example: https://www.tweedekamer.nl/kamerstukken/stemmingsuitslagen/detail?id=2016P10154
+    :return: a list of VotingResult
+    """
     page = requests.get(votings_page_url)
     tree = lxml.html.fromstring(page.content)
     search_results = tree.xpath('//ul[@class="search-result-list reset"]/li')
 
     votings = []
     for search_result in search_results:
-        result = VotingResults(search_result)
+        result = VotingResult(search_result)
         votings.append(result)
     return votings
