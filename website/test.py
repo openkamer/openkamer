64d5cd1db66ee0473893d69364eea290d4d0516f
Bart Römgens <bart.romgens@gmail.com>
2016-09-14 12:08:49 +0200

    create tests for parliament members, extra dossier and political parties

--- website/test.py
+++ website/test.py
@@ -1,14 +1,23 @@
 from django.test import TestCase
 
-from scraper.political_parties import create_parties
-
 from website.create import create_or_update_dossier
+
+from scraper import political_parties
+from scraper import parliament_members
+
+
+class TestCreateParliament(TestCase):
+
+    def test_create_parliament(self):
+        political_parties.create_parties()
+        parliament_members.create_members()
 
 
 class TestCreateDossier(TestCase):
-    dossier_id = 33885
+    fixtures = ['parliament_2016.json']
 
-    def test_create_dossier(self):
-        # TODO: tests takes a bit long, find smaller dossier or use mock objects
-        create_parties()
-        create_or_update_dossier(self.dossier_id)
+    def test_create_dossier_33885(self):
+        create_or_update_dossier(33885)
+
+    def test_create_dossier_33506(self):
+        create_or_update_dossier(33506)
