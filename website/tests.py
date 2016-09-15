a3de623b71642bff919ccc5d86eee44e786472fb
Bart Römgens <bart.romgens@gmail.com>
2016-09-15 12:28:41 +0200

    create tests for person search

--- website/tests.py
+++ website/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
 
-from website.create import create_or_update_dossier
-
 from scraper import political_parties
 from scraper import parliament_members
+
+from parliament.models import ParliamentMember
+
+from website.create import create_or_update_dossier
 
 
 class TestCreateParliament(TestCase):
@@ -21,3 +23,24 @@ class TestCreateDossier(TestCase):
 
     def test_create_dossier_33506(self):
         create_or_update_dossier(33506)
+
+
+class TestFindParliamentMembers(TestCase):
+    fixtures = ['parliament_2016.json']
+
+    def test_find_member(self):
+        surname = 'Zijlstra'
+        forename = 'Halbe'
+        initials = 'H.'
+        member = ParliamentMember.find(surname=surname, initials=initials)
+        self.assertEqual(member.person.forename, forename)
+
+    def test_find_member_surname_prefix(self):
+        surname = 'Weyenberg van'
+        forename = 'Steven'
+        initials = 'S.P.R.A.'
+        member = ParliamentMember.find(surname=surname, initials=initials)
+        self.assertEqual(member.person.forename, forename)
+        surname = 'van Weyenberg'
+        member = ParliamentMember.find(surname=surname, initials=initials)
+        self.assertEqual(member.person.forename, forename)
