cf81d6c56ef0a6cc6badaa754d3276ff3e22dffe
Bart Römgens <bart.romgens@gmail.com>
2016-09-16 17:17:09 +0200

    remove obsolete management commands

--- website/management/commands/create_political_parties.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-
-import scraper.political_parties
-
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        scraper.political_parties.create_parties()
