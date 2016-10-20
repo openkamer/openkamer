3ee5265a6ed110be3cde112d6ffd4161060f0da3
Bart Römgens <bart.romgens@gmail.com>
2016-10-20 14:42:15 +0200

    create government view and template

--- government/views.py
+++ government/views.py
@@ -11,3 +11,11 @@ class GovernmentsView(TemplateView):
         context['governments'] = Government.objects.all()
         return context
 
+
+class GovernmentView(TemplateView):
+    template_name = 'government/government.html'
+
+    def get_context_data(self, government_id, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['government'] = Government.objects.get(id=government_id)
+        return context
