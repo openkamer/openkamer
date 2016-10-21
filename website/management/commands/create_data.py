2c98f1b95d2e9017f615b4f8159c1b96573b3303
Bart Römgens <bart.romgens@gmail.com>
2016-10-21 17:46:14 +0200

    fix call function that does not exist

--- website/management/commands/create_data.py
+++ website/management/commands/create_data.py
@@ -6,13 +6,14 @@ import scraper.government
 
 from person.models import Person
 from website.create import create_or_update_dossier
+from website.create import create_governments
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         scraper.political_parties.create_parties()
-        scraper.government.create_governments()
+        create_governments()
         scraper.parliament_members.create_members()
         Person.update_persons_all('nl')
 
