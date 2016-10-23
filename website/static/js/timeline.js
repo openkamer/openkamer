b6527a8e59b1c7d462c053a5da14820075585dd1
Bart Römgens <bart.romgens@gmail.com>
2016-10-23 21:38:11 +0200

    show new timeline elements a bit earlier
    To make it more clear that there are more items when scrolling down.

--- website/static/js/timeline.js
+++ website/static/js/timeline.js
@@ -1,13 +1,13 @@
 jQuery(document).ready(function($){
 	var timelineBlocks = $('.cd-timeline-block'),
-		offset = 0.8;
+		offset = 0.9;
 
 	//hide timeline blocks which are outside the viewport
 	hideBlocks(timelineBlocks, offset);
 
 	//on scolling, show/animate timeline blocks when enter the viewport
 	$(window).on('scroll', function(){
-		(!window.requestAnimationFrame) 
+		(!window.requestAnimationFrame)
 			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
 			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
 	});
