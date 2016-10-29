51df9312bf8e8b7178461c252ec991adb65984b5
Bart Römgens <bart.romgens@gmail.com>
2016-10-29 23:01:46 +0200

    create management command to no (re)create existing dossiers

--- website/management/commands/create_dossiers_wetsvoorstellen.py
+++ website/management/commands/create_dossiers_wetsvoorstellen.py
@@ -3,6 +3,8 @@ import logging
 
 from django.core.management.base import BaseCommand
 
+from document.models import Dossier
+
 from website.create import create_or_update_dossier, create_dossier_retry_on_error
 
 logger = logging.getLogger(__name__)
@@ -11,16 +13,31 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     MAX_TRIES = 3
 
+    def add_arguments(self, parser):
+        # Named (optional) arguments
+        parser.add_argument(
+            '--skip-existing',
+            action='store_true',
+            dest='skip-existing',
+            default=False,
+            help='Do not create dossiers that already exist.',
+        )
+
     def handle(self, *args, **options):
-        self.create_dossiers_from_file('data/dossier_ids_wetsvoorstellen_initiatief.txt')
-        self.create_dossiers_from_file('data/dossier_ids_wetsvoorstellen_regering.txt')
+        self.create_dossiers_from_file('data/dossier_ids_wetsvoorstellen_initiatief.txt', options['skip-existing'])
+        self.create_dossiers_from_file('data/dossier_ids_wetsvoorstellen_regering.txt', options['skip-existing'])
 
     @staticmethod
-    def create_dossiers_from_file(filename):
+    def create_dossiers_from_file(filename, skip_existing=False):
         with open(filename, 'r') as filein:
             lines = filein.read().splitlines()
             for line in lines:
+                dossier_id = line.strip()
+                dossiers = Dossier.objects.filter(dossier_id=dossier_id)
+                if skip_existing and dossiers.exists():
+                    logger.info('dossier already exists, skip')
+                    continue
                 try:
-                    create_dossier_retry_on_error(dossier_id=line.strip(), max_tries=MAX_TRIES)
+                    create_dossier_retry_on_error(dossier_id=dossier_id, max_tries=Command.MAX_TRIES)
                 except Exception as e:
                     logger.error(traceback.format_exc())
