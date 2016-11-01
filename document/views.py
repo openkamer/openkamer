1bba9cd7965a54010834a0d8b238654813bfd039
Bart Römgens <bart.romgens@gmail.com>
2016-11-01 20:24:21 +0100

    create view and template for timeline besluit item

--- document/views.py
+++ document/views.py
@@ -66,12 +66,42 @@ class DossierView(TemplateView):
 
 
 class TimelineItem(object):
-    def __init__(self, object):
-        self.object = object
+    def __init__(self, obj):
+        self.obj = obj
+
+    @staticmethod
+    def template_name():
+        raise NotImplementedError
+
+    @property
+    def date(self):
+        raise NotImplementedError
+
+
+class TimelineKamerstukItem(TimelineItem):
+    def __init__(self, obj):
+        super().__init__(obj)
 
     @staticmethod
     def template_name():
         return 'document/items/timeline_kamerstuk.html'
+
+    @property
+    def date(self):
+        return self.obj.document.date_published
+
+
+class TimelineBesluitItem(TimelineItem):
+    def __init__(self, obj):
+        super().__init__(obj)
+
+    @staticmethod
+    def template_name():
+        return 'document/items/timeline_besluit.html'
+
+    @property
+    def date(self):
+        return self.obj.besluit_item.besluiten_lijst.date_published
 
 
 class DossierTimelineView(TemplateView):
@@ -83,8 +113,10 @@ class DossierTimelineView(TemplateView):
         context['dossier'] = dossier
         timeline_items = []
         for kamerstuk in dossier.kamerstukken():
-            timeline_items.append(TimelineItem(kamerstuk))
-        print(len(timeline_items))
+            timeline_items.append(TimelineKamerstukItem(kamerstuk))
+        for case in dossier.besluitenlijst_cases():
+            timeline_items.append(TimelineBesluitItem(case))
+        timeline_items = sorted(timeline_items, key=lambda items: items.date, reverse=True)
         context['timeline_items'] = timeline_items
         return context
 
