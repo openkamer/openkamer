d5da6933eaed7a3b489b6c664acd1bd29541668b
Bart Römgens <bart.romgens@gmail.com>
2016-11-02 09:40:48 +0100

    fix order of kamerstukken in timeline
    By default, show the newest and highest kamerstuk nr on top.

--- document/models.py
+++ document/models.py
@@ -178,7 +178,7 @@ class Kamerstuk(models.Model):
 
     class Meta:
         verbose_name_plural = 'Kamerstukken'
-        ordering = ['document__date_published', 'id_sub',]
+        ordering = ['-document__date_published', '-id_sub',]
 
 
 class Agenda(models.Model):
