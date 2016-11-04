b2e1a00b21f673ad8cac84f57cbe07431814226a
Bart Römgens <bart.romgens@gmail.com>
2016-11-04 15:22:42 +0100

    create management command to delete all dossiers

--- website/management/commands/delete_dossiers.py
+++ website/management/commands/delete_dossiers.py
@@ -2,7 +2,7 @@ import logging
 
 from django.core.management.base import BaseCommand
 
-from document.models import BesluitenLijst
+from document.models import Dossier
 
 logger = logging.getLogger(__name__)
 
@@ -10,4 +10,4 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        BesluitenLijst.objects.all().delete()
+        Dossier.objects.all().delete()
