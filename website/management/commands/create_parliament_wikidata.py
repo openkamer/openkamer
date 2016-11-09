9bdee86bd22af7f8300629169acb0336508b9937
Bart Römgens <bart.romgens@gmail.com>
2016-11-09 17:31:33 +0100

    create managmement command to create parliament members from wikidata

---
+++ website/management/commands/create_parliament_wikidata.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+
+import website.create
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        website.create.create_parliament_members_from_wikidata()
