4edc0862ffef5d6764fabfa60c6b001641976c89
Bart Römgens <bart.romgens@gmail.com>
2016-11-13 20:55:09 +0100

    extract two check functions to improve readability

--- parliament/check.py
+++ parliament/check.py
@@ -3,6 +3,8 @@ import json
 import logging
 import os
 
+from unidecode import unidecode
+
 logger = logging.getLogger(__name__)
 
 
@@ -25,3 +27,39 @@ def check_parliament_members_at_date(check_date):
             if start_date < check_date < end_date:
                 members_active.append(member)
     return members_active
+
+
+def get_members_missing(members_current, members_current_check):
+    members_missing = []
+    for member_check in members_current_check:
+        found = False
+        member_check_name = unidecode(member_check['name'])
+        member_check_forename = unidecode(member_check['forename'])
+        for member in members_current:
+            member_name = unidecode(member.person.surname_including_prefix())
+            if member_check_name == member_name and member_check_forename == unidecode(member.person.forename):
+                found = True
+                break
+        if not found:
+            members_missing.append(
+                member_check['initials'] + ' ' + member_check['name'] + ' (' + member_check['forename'] + ')')
+            # print(member_check['name'])
+    return members_missing
+
+
+def get_members_incorrect(members_current, members_current_check):
+    members_incorrect = []
+    for member in members_current:
+        found = False
+        member_name = unidecode(member.person.surname_including_prefix())
+        member_forename = unidecode(member.person.forename)
+        for member_check in members_current_check:
+            member_check_name = unidecode(member_check['name'])
+            member_check_forename = unidecode(member_check['forename'])
+            if member_check_name == member_name and member_check_forename == member_forename:
+                found = True
+                break
+        if not found:
+            members_incorrect.append(member)
+            # print(member.person.fullname())
+    return members_incorrect
--- parliament/views.py
+++ parliament/views.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView
 from parliament.models import Parliament
 from parliament.models import ParliamentMember
 from parliament.models import PoliticalParty
-from parliament.check import check_parliament_members_at_date
+from parliament import check
 
 
 class PartiesView(TemplateView):
@@ -74,38 +74,13 @@ class ParliamentMembersCheckView(TemplateView):
         check_date = datetime.date.today()
         # check_date = datetime.date(year=2008, month=7, day=1)
         members_current = parliament.get_members_at_date(check_date)
-        members_current_check = check_parliament_members_at_date(check_date)
-        members_missing = []
-        for member_check in members_current_check:
-            found = False
-            member_check_name = unidecode(member_check['name'])
-            member_check_forename = unidecode(member_check['forename'])
-            for member in members_current:
-                member_name = unidecode(member.person.surname_including_prefix())
-                if member_check_name == member_name and member_check_forename == unidecode(member.person.forename):
-                    found = True
-                    break
-            if not found:
-                members_missing.append(member_check['initials'] + ' ' + member_check['name'] + ' (' + member_check['forename'] + ')')
-                # print(member_check['name'])
-        members_wrong = []
-        for member in members_current:
-            found = False
-            member_name = unidecode(member.person.surname_including_prefix())
-            member_forename = unidecode(member.person.forename)
-            for member_check in members_current_check:
-                member_check_name = unidecode(member_check['name'])
-                member_check_forename = unidecode(member_check['forename'])
-                if member_check_name == member_name and member_check_forename == member_forename:
-                    found = True
-                    break
-            if not found:
-                members_wrong.append(member)
-                # print(member.person.fullname())
+        members_current_check = check.check_parliament_members_at_date(check_date)
+        members_missing = check.get_members_missing(members_current, members_current_check)
+        members_incorrect = check.get_members_incorrect(members_current, members_current_check)
         context['members'] = members
         context['members_current'] = sorted(members_current, key=lambda member: member.person.surname)
         context['members_current_missing'] = members_missing
-        context['members_current_wrong'] = sorted(members_wrong, key=lambda member: member.person.surname)
+        context['members_incorrect'] = sorted(members_incorrect, key=lambda member: member.person.surname)
         context['members_overlap'] = ParliamentMember.objects.filter(pk__in=overlap_ids).distinct()
         context['members_per_month'] = members_per_month
         return context
@@ -129,3 +104,4 @@ class ParliamentMembersCheckView(TemplateView):
             'members': members,
         })
         return members_per_month
+
