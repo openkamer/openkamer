1bd8c5e91eb01a83d51ed1e29af447cd51a2e463
Bart Römgens <bart.romgens@gmail.com>
2016-11-15 09:00:53 +0100

    create kamerstuk metadata scraper test

--- scraper/tests.py
+++ scraper/tests.py
@@ -230,4 +230,30 @@ class TestVotingScraper(TestCase):
             results += scraper.votings.get_votings_for_page(url)
 
 
+class TestDocumentScraper(TestCase):
 
+    def test_document_metadata_kamerstuk_2016(self):
+        document_id = 'kst-34575-2'
+        metadata = scraper.documents.get_metadata(document_id)
+        self.assertEqual(metadata['publication_type'], 'Kamerstuk')
+        self.assertEqual(metadata['dossier_id'], '34575')
+        self.assertEqual(metadata['date_published'], '2016-10-13')
+        self.assertEqual(metadata['title_short'], 'Voorstel van wet')
+        self.assertEqual(metadata['title_full'], 'Wijziging van de Zorgverzekeringswet en de Wet op de zorgtoeslag in verband met enkele inhoudelijke en technische verbeteringen (Verzamelwet Zvw 2016); Voorstel van wet; Voorstel van wet')
+        self.assertEqual(metadata['category'], 'Zorg en gezondheid | Ziekten en behandelingen')
+        self.assertEqual(metadata['publisher'], 'Tweede Kamer der Staten-Generaal')
+        self.assertEqual(metadata['submitter'], 'E.I. Schippers')
+        self.assertEqual(metadata['publication_type'], 'Kamerstuk')
+
+    def test_document_metadata_kamerstuk_2009(self):
+        document_id = 'kst-32203-2'
+        metadata = scraper.documents.get_metadata(document_id)
+        self.assertEqual(metadata['publication_type'], 'Kamerstuk')
+        self.assertEqual(metadata['dossier_id'], '32203')
+        self.assertEqual(metadata['date_published'], '2009-11-06')
+        self.assertEqual(metadata['title_short'], 'officiële publicatie')
+        self.assertEqual(metadata['title_full'], 'Voorstel van Wet van de leden Van der Ham, De Wit en Teeven tot wijziging van het Wetboek van Strafrecht in verband met het laten vervallen van het verbod op godslastering; Voorstel van wet')
+        self.assertEqual(metadata['category'], 'Recht | Strafrecht|Recht | Staatsrecht|Cultuur en recreatie | Cultuur')
+        self.assertEqual(metadata['publisher'], 'Tweede Kamer der Staten-Generaal')
+        self.assertEqual(metadata['submitter'], 'Ham van der B.|Wit de J.M.A.M.|Teeven F.')
+        self.assertEqual(metadata['publication_type'], 'Kamerstuk')
