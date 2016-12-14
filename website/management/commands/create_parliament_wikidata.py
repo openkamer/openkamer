48a67de5e3766dedd4c378a575d1412c7e85a2d0
Bart Römgens <bart.romgens@gmail.com>
2016-12-14 14:22:02 +0100

    rename create parliament management command
    wikidata replaces tweedekamer.nl as default source

--- website/management/commands/create_parliament_wikidata.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-
-import website.create
-
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        website.create.create_parliament_members()
