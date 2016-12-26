a40c9028b75f7e597e7466ea7db3a382fb3d6b4e
Bart Römgens <bart.romgens@gmail.com>
2016-12-26 15:10:41 +0100

    order submitter party filter field by number of seats

--- stats/filters.py
+++ stats/filters.py
@@ -15,13 +15,15 @@ logger = logging.getLogger(__name__)
 
 
 class PartyVotesFilter(django_filters.FilterSet):
+    BILL = 'wetsvoorstel'
+    OTHER = 'overig'
     VOTING_TYPE_CHOICES = (
-        ('BILL', 'Wetsvoorstel'),
-        ('OTHER', 'Other (Moties, Amendementen)')
+        (BILL, 'Wetsvoorstel'),
+        (OTHER, 'Overig (Motie, Amendement)')
     )
     type = django_filters.ChoiceFilter(method='type_filter', choices=VOTING_TYPE_CHOICES)
     submitter = django_filters.ModelChoiceFilter(
-        queryset=PoliticalParty.objects.all(),
+        queryset=PoliticalParty.objects.all().order_by('-current_parliament_seats'),
         to_field_name='slug',
         method='submitter_party_filter',
         label='',
@@ -32,9 +34,9 @@ class PartyVotesFilter(django_filters.FilterSet):
         exclude = '__all__'
 
     def type_filter(selfs, queryset, name, value):
-        if value == 'BILL':
+        if value == PartyVotesFilter.BILL:
             return queryset.filter(voting__is_dossier_voting=True).distinct()
-        elif value == 'OTHER':
+        elif value == PartyVotesFilter.OTHER:
             return queryset.filter(voting__is_dossier_voting=False).distinct()
 
     def submitter_party_filter(self, queryset, name, value):
