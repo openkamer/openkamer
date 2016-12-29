aa5cad970c8acb5afb1cfa4a1ab46940f8680206
Bart Römgens <bart.romgens@gmail.com>
2016-12-29 14:06:43 +0100

    create management command to delete dossiers that are not bills

---
+++ document/management/commands/delete_dossiers_no_wetsvoorstel.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+from document.models import Dossier
+
+from django.db import transaction
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        self.do()
+
+    @transaction.atomic
+    def do(self):
+        wetsvoorstel_ids = Dossier.get_active_dossier_ids()
+        wetsvoorstel_ids += Dossier.get_inactive_dossier_ids()
+        Dossier.objects.exclude(dossier_id__in=wetsvoorstel_ids).delete()
