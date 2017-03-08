1dd2521f4a4117595456cbace706ed25bea7abd5
Bart Römgens <bart.romgens@gmail.com>
2017-03-08 13:07:58 +0100

    display the kamervraag time to answer

--- document/views.py
+++ document/views.py
@@ -349,7 +349,7 @@ class KamervragenView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        kamervragen = Kamervraag.objects.all()
+        kamervragen = Kamervraag.objects.all()[:10]
         context['kamervragen'] = kamervragen
         return context
 
