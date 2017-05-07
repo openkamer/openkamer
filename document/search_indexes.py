4ef0a84fda0ab6586bde5444a286d114f93e55cd
Stef Lommen <github@steflommen.com>
2017-05-08 00:30:18 +0200

    removes document from search index

--- document/search_indexes.py
+++ document/search_indexes.py
@@ -9,22 +9,6 @@ import datetime
 from haystack import indexes
 from document.models import Document, Kamerstuk,Kamervraag
 
-#
-class DocumentIndex(indexes.SearchIndex, indexes.Indexable):
-    #text = indexes.CharField(document=True, use_template=True)
-    title = indexes.CharField(model_attr='title_full')
-    text = indexes.CharField(use_template=True,document=True)
-    #slug = indexes.CharField(model_attr='slug')
-    publication_type = indexes.CharField(model_attr='publication_type', faceted=True )
-#    date_published = indexes.DateField(model_attr='date_published')
-
-
-    def get_model(self):
-        return Document
-
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        return self.get_model().objects.all()
         
 class KamerstukIndex(indexes.SearchIndex, indexes.Indexable):
     title = indexes.CharField(model_attr='document')
