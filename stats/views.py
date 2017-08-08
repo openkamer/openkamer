65ddcb825324b37105b82cc04ff3fbc161bad897
Bart Römgens <bart.romgens@gmail.com>
2017-08-08 18:09:28 +0200

    create three concept kamervraag plots
    - kamervragen vs time
    - kamervraag reply time
    - kamervraag reply time vs time (contour)

--- stats/views.py
+++ stats/views.py
@@ -1,11 +1,13 @@
 import json
 import random
 import logging
+import datetime
 from urllib.parse import urlparse
 
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 from django.utils.safestring import mark_safe
+from django.utils import timezone
 
 from person.models import Person
 
@@ -151,6 +153,153 @@ class KamervraagFootnotesView(TemplateView):
                 domains[hostname] = 1
         domains_sorted = [(k, domains[k]) for k in sorted(domains, key=domains.get, reverse=True)]
         return domains_sorted
+
+
+class KamervraagVSTime(TemplateView):
+    template_name = "stats/kamervraag_vs_time.html"
+
+    def get_context_data(self, **kwargs):
+        import plotly.offline
+        from plotly.graph_objs import Scatter, Layout, Histogram
+
+        kamervragen = Kamervraag.objects.all().select_related('document')
+        kamervraag_dates = []
+        for kamervraag in kamervragen:
+            kamervraag_dates.append(kamervraag.document.date_published)
+
+        plot_html = plotly.offline.plot(
+            figure_or_data={
+                # "data": [Scatter(x=data_x, y=data_y)],
+                "data": [Histogram(
+                    x=kamervraag_dates,
+                    autobinx=False,
+                    xbins=dict(
+                        start=(timezone.now() - datetime.timedelta(days=7*365)).timestamp()*1000,
+                        end=timezone.now().timestamp()*1000,
+                        size=60*60*24*7*1000
+                    ),
+                )],
+                "layout": Layout(
+                    title="Kamervragen per Week",
+                    xaxis=dict(title='Tijd'),
+                    yaxis=dict(title='Kamervragen per week'),
+                )
+            },
+            show_link=False,
+            output_type='div',
+            include_plotlyjs=False,
+            auto_open=False,
+        )
+        context = super().get_context_data(**kwargs)
+        context['plot_html'] = mark_safe(plot_html)
+        return context
+
+
+class KamervraagReplyTime(TemplateView):
+    template_name = "stats/kamervraag_reply_time.html"
+
+    def get_context_data(self, **kwargs):
+        kamervragen = Kamervraag.objects.filter(kamerantwoord__isnull=False).select_related('document')
+        kamervraag_durations = []
+        for kamervraag in kamervragen:
+            kamervraag_durations.append(kamervraag.duration)
+        context = super().get_context_data(**kwargs)
+        context['plot_html'] = mark_safe(KamervraagReplyTime.create_plot(kamervraag_durations))
+        return context
+
+    @staticmethod
+    def create_plot(kamervraag_durations):
+        import plotly.offline
+        from plotly.graph_objs import Layout, Histogram
+        return plotly.offline.plot(
+            figure_or_data={
+                # "data": [Scatter(x=data_x, y=data_y)],
+                "data": [Histogram(
+                    x=kamervraag_durations,
+                    autobinx=False,
+                    xbins=dict(
+                        start=0,
+                        end=100,
+                        size=1
+                    ),
+                )],
+                "layout": Layout(
+                    title="Dagen tot Antwoord",
+                    xaxis=dict(title='Antwoordtijd [dagen]'),
+                    yaxis=dict(title='Aantal Kamervragen'),
+                ),
+            },
+            show_link=False,
+            output_type='div',
+            include_plotlyjs=False,
+            auto_open=False,
+        )
+
+
+class KamervraagReplyTimeContour(TemplateView):
+    template_name = "stats/kamervraag_reply_time_contour.html"
+
+    def get_context_data(self, **kwargs):
+        kamervragen = Kamervraag.objects.filter(kamerantwoord__isnull=False).select_related('document')
+        kamervraag_dates = []
+        for kamervraag in kamervragen:
+            kamervraag_dates.append(kamervraag.document.date_published)
+        kamervraag_durations = []
+        for kamervraag in kamervragen:
+            kamervraag_durations.append(kamervraag.duration)
+        context = super().get_context_data(**kwargs)
+        context['plot_html'] = mark_safe(KamervraagReplyTimeContour.create_plot(kamervraag_dates, kamervraag_durations))
+        return context
+
+    @staticmethod
+    def create_plot(kamervraag_dates, kamervraag_duration):
+        import plotly.offline
+        from plotly.graph_objs import Layout, Histogram2d
+        data = [
+            Histogram2d(
+                x=kamervraag_dates,
+                y=kamervraag_duration,
+                histnorm='',
+                autobinx=False,
+                xbins=dict(
+                    start=(timezone.now() - datetime.timedelta(days=7 * 365)).timestamp() * 1000,
+                    end=timezone.now().timestamp() * 1000,
+                    size=60*60*24*14*1000
+                ),
+                autobiny=False,
+                ybins=dict(
+                    start=0,
+                    end=80,
+                    size=3
+                ),
+                colorscale='Jet',
+                # colorscale='Viridis',
+                # colorscale=[[0, 'rgb(12,51,131)'], [0.25, 'rgb(10,136,186)'], [0.5, 'rgb(242,211,56)'],
+                #            [0.75, 'rgb(242,143,56)'], [1, 'rgb(217,30,30)']],
+                zsmooth='best',
+                zauto=False,
+                zmin=0,
+                zmax=21,
+            ),
+        ]
+        return plotly.offline.plot(
+            figure_or_data={
+                # "data": [Scatter(x=data_x, y=data_y)],
+                "data": data,
+                "layout": Layout(
+                    title="Kamervraag Antwoordtijd vs Tijd",
+                    xaxis=dict(title='Kamervraag Ingediend [tijd]'),
+                    yaxis=dict(title='Antwoordtijd [dagen]'),
+                    autosize=False,
+                    width=1000,
+                    height=500,
+                ),
+            },
+            show_link=False,
+            output_type='div',
+            include_plotlyjs=False,
+            auto_open=False,
+        )
 
 
 def get_example_plot_html(number_of_points=30):
