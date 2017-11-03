24674af83403f585c70d06754dfe2e6a311510f5
Bart Römgens <bart.romgens@gmail.com>
2017-11-03 13:13:04 +0100

    Make clean transaction atomic

--- document/management/commands/clean_submitters.py
+++ document/management/commands/clean_submitters.py
@@ -4,18 +4,18 @@ from django.core.management.base import BaseCommand
 from django.db import transaction
 from django.db import models
 
-from document.models import Document
 from document.models import Submitter
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    """Remove duplicate submitters"""
 
     def handle(self, *args, **options):
         self.do()
 
-    # @transaction.atomic
+    @transaction.atomic
     def do(self):
         unique_fields = ['person', 'document']
 
