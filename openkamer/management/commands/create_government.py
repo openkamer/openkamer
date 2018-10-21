612207f0f8b104dfd8f9c99e62e01fe07168edb5
Bart Römgens <bart.romgens@gmail.com>
2018-10-21 19:30:59 +0200

    Add government Rutte-III to create governments command

--- openkamer/management/commands/create_government.py
+++ openkamer/management/commands/create_government.py
@@ -7,6 +7,7 @@ class Command(BaseCommand):
     # Balkenende IV : Q1719725
     # Rutte I : Q168828
     # Rutte II : Q1638648
+    # Rutte III : Q42293409
 
     def add_arguments(self, parser):
         parser.add_argument('wikidata_id', nargs='+', type=str)
