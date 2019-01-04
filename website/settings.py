10f6fa0e24210a4eac679b38f3b4bd06df61de39
Bart Römgens <bart.romgens@gmail.com>
2019-01-04 01:19:55 +0100

    Restrict unauthenticated users to readonly for rest-framework

--- website/settings.py
+++ website/settings.py
@@ -156,7 +156,10 @@ HAYSTACK_CONNECTIONS = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    )
 }
 
 ###########
