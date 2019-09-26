e3c702f26ff7149480a5bf904d62cc0c41c8fc74
Bart Römgens <bart.romgens@gmail.com>
2019-09-26 21:46:43 +0200

    Remove unused apps config

--- document/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DocumentsConfig(AppConfig):
-    name = 'document'
--- parliament/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ParliamentConfig(AppConfig):
-    name = 'parliament'
