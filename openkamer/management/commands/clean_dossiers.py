97ac489c6b9aeecc386c1f08faf9ef903a04b2dc
Bart Römgens <bart.romgens@gmail.com>
2019-11-29 16:17:53 +0100

    Update clean dossiers command

--- openkamer/management/commands/clean_dossiers.py
+++ openkamer/management/commands/clean_dossiers.py
@@ -11,14 +11,15 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        dossier_ids = get_dossier_ids()
-        if len(dossier_ids) < 1500:
+        dossiers = get_dossier_ids()
+        if len(dossiers) < 1500:
             logger.error('Less than 1500 dossiers found, something wrong, abort!')
             return
-        dossiers_to_delete = []
-        for dossier_id in dossier_ids:
-            dossier_id = Dossier.create_dossier_id(dossier_id.dossier_id, dossier_id.dossier_sub_id)
-            dossiers_to_delete += Dossier.objects.exclude(dossier_id=dossier_id)
+        dossiers_valid = []
+        for dossier in dossiers:
+            dossier_id = Dossier.create_dossier_id(dossier.dossier_id, dossier.dossier_sub_id)
+            dossiers_valid.append(dossier_id)
 
+        dossiers_to_delete = Dossier.objects.exclude(dossier_id__in=dossiers_valid)
         logger.info('Deleting ' + str(len(dossiers_to_delete)) + ' dossiers and related items')
         dossiers_to_delete.delete()
