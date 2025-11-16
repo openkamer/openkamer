a93e57ed8bf3725b616db1bd94281b8ccb23f87d
Bart Römgens <bart.romgens@gmail.com>
2025-11-16 16:28:12 +0100

    Fix (IV) duplicate key value violates unique constraint on updating submitters

--- openkamer/document.py
+++ openkamer/document.py
@@ -5,6 +5,7 @@ import lxml
 import re
 
 from django.db import transaction
+from django.db import IntegrityError
 from django.urls import resolve, Resolver404
 from django.urls import reverse
 
@@ -281,15 +282,23 @@ class SubmitterFactory(object):
 
         person = SubmitterFactory.get_person(document, tk_person, name)
         party_slug = SubmitterFactory.get_party_slug(person, document)
-        submitter, created = Submitter.objects.get_or_create(
-            person=person, document=document, type=submitter_type,
-            defaults={'party_slug': party_slug}
-        )
-        if not created:
-            if submitter.party_slug != party_slug:
-                submitter.party_slug = party_slug
-                submitter.save()
-        return submitter
+        
+        try:
+            submitter, created = Submitter.objects.get_or_create(
+                person=person, document=document, type=submitter_type,
+                defaults={'party_slug': party_slug}
+            )
+            if not created:
+                if submitter.party_slug != party_slug:
+                    submitter.party_slug = party_slug
+                    submitter.save()
+            return submitter
+        except IntegrityError as e:
+            # Document was deleted between fetch and insert
+            if 'document_submitter_document_id' in str(e):
+                logger.exception('Document {} was deleted during submitter creation, skipping'.format(document.document_id))
+                return None
+            raise
 
     @staticmethod
     def get_active_persons(date):
